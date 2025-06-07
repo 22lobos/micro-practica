51970e46588942dbfe94c8812351dcac7c1f948b
22lobos <cr.loboso@duocuc.cl>
2025-06-06 21:34:01 -0400

    Poblando model

--- src/main/java/com/example/crudrapido/model/Estudiante.java
+++ src/main/java/com/example/crudrapido/model/Estudiante.java
@@ -1,5 +1,21 @@
 package com.example.crudrapido.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "estudiantes")
 public class Estudiante {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private String apellido;
+    private String email;
 }
